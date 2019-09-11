8f4f097abb35627a8cd01f3f9aef0e052fdb069c
rhanetuk <46538913+rhanetuk@users.noreply.github.com>
2019-09-11 16:29:36 +0100

    Update rhanetinstall.sh

--- rhanetinstall.sh
+++ rhanetinstall.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 echo jedimakerxtream Install
 clear
 echo -------------------------------------------------------------------------------
@@ -13,7 +12,7 @@ echo Removing Old Bouquet Makers...
 opkg remove enigma2-plugin-aceiptv-bouquetmaker
 opkg remove enigma2-plugin-extensions-e2m3u2bouquet
 echo Installing New Bouquet Makers...
-wget https://github.com/rhanetuk/Software/blob/master/enigma2-plugin-extensions-jedimakerxtream_5.17-20190904_all.ipk
-opkg install /tmp/enigma2-plugin-extensions-jedimakerxtream_5.17-20190904_all.ipk
+wget https://github.com/rhanetuk/Software/blob/master/jedimaker.ipk
+opkg install /tmp/https://github.com/rhanetuk/Software/blob/master/jedimaker.ipk
 rm *.xml -f
 shutdown -r now
