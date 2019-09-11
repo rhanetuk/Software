b65a87c511e028a4bd313038d8eb70fcabcf1277
rhanetuk <46538913+rhanetuk@users.noreply.github.com>
2019-09-11 12:43:35 +0100

    Update rhanetinstall.sh

--- rhanetinstall.sh
+++ rhanetinstall.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# Solar Install v0.7
+echo jedimakerxtream Install
 clear
-echo Running Solar Install v0.7
-echo IF YOU HAVE ISSUES, PLEASE CONTACT SUPPORT AND LET THEM KNOW YOU ARE USING v0.7
 echo -------------------------------------------------------------------------------
 echo Downloading, please wait...
 echo
@@ -11,14 +9,11 @@ echo
 cd /
 cd tmp
 rm *.ipk -f
-rm *.xml -f
-opkg update
 echo Removing Old Bouquet Makers...
 opkg remove enigma2-plugin-aceiptv-bouquetmaker
 opkg remove enigma2-plugin-extensions-e2m3u2bouquet
 echo Installing New Bouquet Makers...
 wget https://github.com/rhanetuk/Software/blob/master/enigma2-plugin-extensions-jedimakerxtream_5.17-20190904_all.ipk
-wget http://5.101.146.38/config.xml
 opkg install /tmp/enigma2-plugin-extensions-jedimakerxtream_5.17-20190904_all.ipk
 rm *.xml -f
 shutdown -r now
