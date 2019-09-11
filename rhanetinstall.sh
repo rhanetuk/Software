e3b8311be1e23616380f9e03a5e780fad64c254b
rhanetuk <46538913+rhanetuk@users.noreply.github.com>
2019-09-11 12:36:51 +0100

    Update rhanetinstall.sh

--- rhanetinstall.sh
+++ rhanetinstall.sh
@@ -17,20 +17,8 @@ echo Removing Old Bouquet Makers...
 opkg remove enigma2-plugin-aceiptv-bouquetmaker
 opkg remove enigma2-plugin-extensions-e2m3u2bouquet
 echo Installing New Bouquet Makers...
-wget https://github.com/rhanetuk/Software/blob/master/enigma2-plugin-e2m3u2bouquet_0.8.2_all.ipk
+wget https://github.com/rhanetuk/Software/blob/master/enigma2-plugin-extensions-jedimakerxtream_5.17-20190904_all.ipk
 wget http://5.101.146.38/config.xml
-opkg install /tmp/enigma2-plugin-e2m3u2bouquet_0.8.2_all.ipk
-cd /etc/enigma2/e2m3u2bouquet
+opkg install /tmp/enigma2-plugin-extensions-jedimakerxtream_5.17-20190904_all.ipk
 rm *.xml -f
-echo
-echo Grabbing Configuration from SOLAR
-echo =====================================================
-echo Using IP Address to get around people with DNS Issues
-echo Or Safe-Browsing On with ISP.
-echo =====================================================
-wget http://5.101.146.38/config.xml
-echo
-echo
-echo Finished install, Waiting to reboot - Install v0.7
-echo
 shutdown -r now
